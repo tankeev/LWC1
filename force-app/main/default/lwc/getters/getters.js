9d8d13bce287fb499611a2a6ef4e91cdfd0de247
tankeev <tankeev@gmail.com>
2022-07-18 14:09:50 +0600

    lwc input values are entered. the output function will display the sum of entered numbers

--- force-app/main/default/lwc/getters/getters.js
+++ force-app/main/default/lwc/getters/getters.js
@@ -1,10 +1,30 @@
 import { LightningElement } from 'lwc';
 
 export default class Getters extends LightningElement {
-fname = 'john';
-lname = 'nash';
+fname = '';
+lname = '';
+
+num1 = 12;
+num2 = 2;
 
 get fullNameUpperCs(){
     return `${this.fname.toUpperCase()} ${this.lname.toUpperCase()}`
 }
+
+
+
+handleChange1(event){
+    this.num1 = event.target.value;
+}
+handleChange2(event){
+    this.num2 = event.target.value;
+}
+
+get total(){
+    return `${parseInt(this.num1)+parseInt(this.num2)}`
+}
+
+
+
+
 }
