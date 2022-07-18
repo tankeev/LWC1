cbc0a1ce634d62ad4e45bec700155f115974cceb
tankeev <tankeev@gmail.com>
2022-07-18 16:19:15 +0600

    if Checkbox is selectred show text1 else show text

--- force-app/main/default/lwc/conditional_rendering/conditional_rendering.js
+++ force-app/main/default/lwc/conditional_rendering/conditional_rendering.js
@@ -2,4 +2,9 @@ import { LightningElement } from 'lwc';
 
 export default class Conditional_rendering extends LightningElement {
     isTrue = false;
+    Display = false;
+
+    handleChange(event){
+        this.Display = event.target.checked; 
+    }
 }
