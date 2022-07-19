b6553ff7ce0c2e29491fa3546868c1e99a9ad880
tankeev <tankeev@gmail.com>
2022-07-19 10:54:33 +0600

    order

---
+++ force-app/main/default/lwc/orderComponent/orderComponent.js
@@ -0,0 +1,44 @@
+import { LightningElement } from 'lwc';
+
+export default class OrderComponent extends LightningElement {
+    ingredients =[
+        {
+            id:1,
+            name:'cheese'
+        },
+        {
+            id:2,
+            name:'olive'
+        },
+        {
+            id:3,
+            name:'pepperoni'
+        },
+        {
+            id:4,
+            name:'onion'
+        },]
+    drinks =[
+        {
+            id:1,
+            name:'water'
+        },
+        {
+            id:2,
+            name:'coke'
+        },
+        {
+            id:3,
+            name:'sprite'
+        },
+        {
+            id:4,
+            name:'fanta'
+        },]
+
+    buttonClick;
+
+    handleOrderButton1(event){
+        this.buttonClick = event.target.;
+    }
+}
