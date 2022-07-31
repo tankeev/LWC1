29bcf4171c181efa55a3f8600c439b038e42110a
tankeev <tankeev@gmail.com>
2022-08-01 00:40:02 +0600

    @track reactive

--- force-app/main/default/lwc/decoratorsParent/decoratorsParent.js
+++ force-app/main/default/lwc/decoratorsParent/decoratorsParent.js
@@ -1,3 +1,12 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class DecoratorsParent extends LightningElement {}
+export default class DecoratorsParent extends LightningElement {
+    @track person = {
+        name: 'azamat',
+        age: 30
+    }
+
+    handlePerson(event){
+        this.person.name = event.target.value
+    }
+}
