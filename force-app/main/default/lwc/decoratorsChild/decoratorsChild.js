6546d1f25363da3b4f21b2ea2bd33423b01cce86
tankeev <tankeev@gmail.com>
2022-08-02 00:56:53 +0600

    implementnted accounts.data into html inside of for loop

--- force-app/main/default/lwc/decoratorsChild/decoratorsChild.js
+++ force-app/main/default/lwc/decoratorsChild/decoratorsChild.js
@@ -1,18 +1,13 @@
 import { LightningElement, wire } from 'lwc';
-import NAME_FIELD from '@salesforce/schema/Account.Name';
-import getAccounts from '@salesforce/apex/AccountController.getAccounts';
+import getAccount from '@salesforce/apex/accountFilter.getAccount';
 
-const COLUMNS = [
-    { label: 'Account Name', fieldName: NAME_FIELD.fieldApiName, type: 'text' },
-];
 
 export default class DecoratorsChild extends LightningElement {
-    columns = COLUMNS;
-    @wire(getAccounts)
-    accounts;
+    filterWord = '';
 
+    @wire(getAccount,{filter:'$filterWord'})accounts;
 
     handleAccount(event){
-         this.COLUMNS=event.target.value
+        this.filterWord = event.target.value
     }
 }
